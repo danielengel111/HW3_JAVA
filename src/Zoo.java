fc47f4e6eac37e7abc467d09a58c119cc5ee05be
StasEngel <69434834+StasEngel@users.noreply.github.com>
2021-06-25 10:19:02 +0300

    .

--- src/Zoo.java
+++ src/Zoo.java
@@ -1,3 +1,24 @@
+import java.util.*;
+
+
 public class Zoo
 {
+    private static Zoo instance = new Zoo();
+    private ArrayList<ZooObserver> observers;
+    private int hunger;
+    private int happiness;
+
+    private Zoo()
+    {
+        this.observers = new ArrayList<>();
+        this.happiness = 2;
+        this.hunger = 3;
+    }
+
+    public static Zoo getInstance() { return instance; }
+
+    public void addObserver(ZooObserver observer)
+    {
+        this.observers.add(observer);
+    }
 }
--- src/ZooObserver.java
+++ src/ZooObserver.java
@@ -1,3 +1,11 @@
 public class ZooObserver
 {
+    private String name;
+
+    public ZooObserver(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName(){ return this.name; }
 }
