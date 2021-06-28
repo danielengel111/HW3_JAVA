d28c1c98493869fa9abb3ad7cc3b5eb8d74d153a
StasEngel <69434834+StasEngel@users.noreply.github.com>
2021-06-29 02:57:28 +0300

    .

--- src/AnimalFactory.java
+++ src/AnimalFactory.java
@@ -1,4 +1,8 @@
 public abstract class AnimalFactory
 {
+    /**
+     * create and return a new animal instance
+     * using the Builder design pattern.
+     */
     public abstract Animal createAnimal();
 }
